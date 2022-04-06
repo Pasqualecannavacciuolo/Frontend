cefc8be41a758fa07c18b8a1643e2d875882fb4b
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-06 14:23:38 +0200

    Added some data structures and input from an alert

--- Esempio Javascript/first.js
+++ Esempio Javascript/first.js
@@ -22,3 +22,20 @@ function conversion(n) {
 conversion("00010101");
 conversion("11001010");
 conversion("10010110");
+
+// Javascript consente di creare un'array misto
+let array = [1, 2, "Ciao", 4, 5];
+array.forEach(function(n) {
+    console.log(n);
+})
+
+// Definizione di una matrice
+let matrix = [
+    []
+];
+matrix.push(1);
+console.log(matrix);
+
+// Input da tastiera mediante un'alert
+let nome = prompt("Mi chiamo")
+console.log("Nome: " + nome)
