ea99c3dc41127e8de48ea6054bd46065ba550564
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-07 09:34:37 +0200

    Accessing a button via selector

--- Javascript/Teoria/Accedere HTML/main.js
+++ Javascript/Teoria/Accedere HTML/main.js
@@ -4,4 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
     let container = document.getElementById('container').classList.add('classAdded');
     let containerTitle = document.getElementsByClassName('cont-title')[0].classList.add('bold');
     let containerParagraph = document.getElementsByClassName('cont-paragraph')[0].classList.add('italic');
+    let containerButton = document.querySelector('.btn').innerHTML = 'Cambiato Premimi';
 })
