001857e58388a1950a1035dd4c5e2be44712f750
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-07 15:56:10 +0200

    Added styling with classes

--- Javascript/Teoria/AJAX/First Example/ajax.js
+++ Javascript/Teoria/AJAX/First Example/ajax.js
@@ -10,17 +10,22 @@ const ajax = () => {
         if (this.readyState == 4) {
             const res = JSON.parse(this.responseText);
             for (let i = 0; i < res.length; i++) {
+                // Wrapper
                 let div = document.createElement('DIV');
                 // Creo i paragrafi
                 let p = document.createElement('P');
+                p.classList.add('userId');
                 let p2 = document.createElement('P');
+                p2.classList.add('Id');
                 let p3 = document.createElement('P');
+                p3.classList.add('title');
                 let p4 = document.createElement('P');
+                p4.classList.add('body');
 
                 // Assegno ad ogni paragrafo un valore
-                p.appendChild(document.createTextNode(res[i].userId));
-                p2.appendChild(document.createTextNode(res[i].id));
-                p3.appendChild(document.createTextNode(res[i].title));
+                p.appendChild(document.createTextNode("UserId: " + res[i].userId));
+                p2.appendChild(document.createTextNode("Id: " + res[i].id));
+                p3.appendChild(document.createTextNode("Title: " + res[i].title));
                 p4.appendChild(document.createTextNode(res[i].body));
 
                 // Aggiungo gli elementi creati al DIV di BASE
