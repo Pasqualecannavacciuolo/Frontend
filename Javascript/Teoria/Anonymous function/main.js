6a86f6540d262f71aa18d739f72dbaaeb4171753
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-07 11:45:51 +0200

    First implementation and examples

---
+++ Javascript/Teoria/Anonymous function/main.js
@@ -0,0 +1,13 @@
+// CLASSIC FUNCTION
+function name(param) {
+    // Do something
+}
+
+// ANONYMOUS FUNCTION assigned to a variable
+let fn = function() {}
+
+// ANONYMOUS FUNCTION executed directly
+// IIFE -> immediately invoked function
+(function() {
+    alert('Ciao mondo');
+}());
