e9fe5b2a1ac82cd234d2e53e03d3659f2eb324a4
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-07 09:28:20 +0200

    Changing content via JS in multiple methods

---
+++ Javascript/Teoria/Accedere HTML/main.js
@@ -0,0 +1,7 @@
+// Appena il DOM è stato caricato fai delle operazioni
+document.addEventListener('DOMContentLoaded', function() {
+
+    let container = document.getElementById('container').classList.add('classAdded');
+    let containerTitle = document.getElementsByClassName('cont-title')[0].classList.add('bold');
+    let containerParagraph = document.getElementsByClassName('cont-paragraph')[0].classList.add('italic');
+})
