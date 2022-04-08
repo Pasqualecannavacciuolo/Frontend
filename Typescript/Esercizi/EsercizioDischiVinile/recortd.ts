4d613425a8c8643ebfd92b49e46d0506cd257378
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-08 15:59:02 +0200

    ADDED CONSTRUCTOR

--- Typescript/Esercizi/EsercizioDischiVinile/recortd.ts
+++ Typescript/Esercizi/EsercizioDischiVinile/recortd.ts
@@ -7,12 +7,25 @@ interface IRecords {
 
 interface IRecordsOperation<Z> {
     loadRecord(): Z;
+    printData<Z>(): void;
 }
 
 class RecordsImpl implements IRecordsOperation<IRecords> {
+    
+    record: IRecords;
+    constructor({ id, name, age, description }: IRecords) {
+        this.record.id = id;
+        this.record.name = name;
+        this.record.age = age;
+        this.record.description = description;
+    }
+    
+    printData<IRecords>(): void {
+        throw new Error("Method not implemented.");
+    }
 
     loadRecord(): IRecords {
         throw new Error("Method not implemented.");
     }
-    
+
 }
